36bf0cd69031b876adbb5b949810e19fbb8d98dd
Ethan Cain <ecain121@gmail.com>
2019-10-02 07:25:39 -0500

    creation of repo

---
+++ Colors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Colors
+//
+//  Created by Ethan Cain on 10/2/19.
+//  Copyright © 2019 Mizzou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
