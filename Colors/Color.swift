48dcbc307e9e5f48cd59f8f95bb42fefe9437b00
Ethan Cain <ecain121@gmail.com>
2019-10-25 22:38:06 -0500

    done

---
+++ Colors/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Colors
+//
+//  Created by Ethan Cain on 10/25/19.
+//  Copyright © 2019 Mizzou. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
